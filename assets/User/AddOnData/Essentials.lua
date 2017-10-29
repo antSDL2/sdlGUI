d102d1c51ea7440cc9cb6e31fb6e11c1848cd9c9
Aristotelis Trapalis <atrapalis1@sheffield.ac.uk>
2017-10-29 16:00:32 +0000

    Added assets folder (push number 2)

---
+++ assets/User/AddOnData/Essentials.lua
@@ -0,0 +1,5 @@
+if (Essentials.SaveData["VSync"] ~= nil) then Essentials.SaveData["VSync"] = false; end
+if (Essentials.SaveData["FullscreenResolution"] ~= nil) then Essentials.SaveData["FullscreenResolution"] = "1920x1080"; end
+if (Essentials.SaveData["WindowMode"] ~= nil) then Essentials.SaveData["WindowMode"] = 0 end;
+if (Essentials.SaveData["WindowedResolution"] ~= nil) then Essentials.SaveData["WindowedResolution"] = "1428x791"; end
+if (Essentials.SaveData["Display"] ~= nil) then Essentials.SaveData["Display"] = 0 end;
